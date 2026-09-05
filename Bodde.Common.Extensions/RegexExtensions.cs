03c6e8cda00e74d8ad955cdf9465dbfc18184c40
bodde <bodde>
2026-09-05 18:11:21 +0200

    Refactor RegexExtensions methods for improved group matching and value extraction (WIP)

--- Bodde.Common.Extensions/RegexExtensions.cs
+++ Bodde.Common.Extensions/RegexExtensions.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text.RegularExpressions;
 
 public static class RegexExtensions
@@ -7,37 +8,62 @@ public static class RegexExtensions
         public string[] MatchingValues(string input, string groupName)
         {
             var matches = regex.Matches(input);
-            var namedGroups = matches
-                .Cast<Match>()
-                .Select(match => match.Groups[groupName])
-                .Where(_ => _.Success)
-                //.Where(_ => _.Success && _.Captures.Count == 1)
-                .ToArray();
-
-            return namedGroups.Select(_ => _.Value).ToArray();
-        }
-
-        public Dictionary<string, string[]> MatchingValues(string input, string[] groupNames)
-        {
-            var matches = regex.Matches(input);
-            var namedGroups = matches
-                .Cast<Match>()
-                .SelectMany(match => GetNamedGroups(match, groupNames))
-                .ToArray();
+            var namedGroups = GetNamedGroups(matches, groupName);
 
             return namedGroups
-                .GroupBy(namedGroup => namedGroup.Name)
+                .Select(group => group.Value)
+                .ToArray();
+        }
+
+        public Dictionary<string, string[]> MatchingGroupsValues(string input, string[] groupNames)
+        {
+            var matches = regex.Matches(input);
+
+            var valuesByGroup = groupNames
+                .SelectMany(groupName => GetNamedGroups(matches, groupName))
+                .GroupBy(group => group.ToString())
                 .ToDictionary(
-                    namedGroups => namedGroups.Key, 
-                    namedGroups => namedGroups.Select(_ => _.Group.Value).ToArray()
+                    groupsByName => groupsByName.Key, 
+                    groupsByName => groupsByName.Select(g => g.Value).ToArray()
                     );
+
+            return valuesByGroup;
+        }
+
+        public T FromMatchingValues<T>(string input, Func<string[], string>? pickOne = null)
+            where T: new()
+        {
+            pickOne ??= values => values.FirstOrDefault();
+
+            var propertyInfos = typeof(T).GetPropertyInfos(flags: BindingFlags.Public | BindingFlags.Instance | BindingFlags.SetField);
+            var propertyNames = propertyInfos.Select(pi => pi.Name).ToArray();
+
+            var matchingGroupValues = regex.MatchingGroupsValues(input, propertyNames);
+
+            var instance = new T();
+            foreach(var propertyInfo in propertyInfos)
+            {
+                if(matchingGroupValues.TryGetValue(propertyInfo.Name, out var groupValues) == false)
+                    continue;
+
+                var groupValue = pickOne(groupValues);
+                if(groupValue == null)
+                    continue;
+
+                var propertyValue = Convert.ChangeType(groupValue, propertyInfo.PropertyType);
+                propertyInfo.SetValue(instance, propertyValue);
+            }
+
+            return instance;
         }
     }
 
-    private static IEnumerable<(string Name, Group Group)> GetNamedGroups(Match match, string[] groupNames)
+    private static Group[] GetNamedGroups(MatchCollection matchCollection, string groupName)
     {
-        return groupNames
-            .Select(name => (Name: name, Group: match.Groups[name]))
-            .Where(namedGroup => namedGroup.Group.Success);
+        return matchCollection
+            .Cast<Match>()
+            .SelectMany(m => m.Groups.Cast<Group>())
+            .Where(g => g.ToString() == groupName)
+            .ToArray();
     }
 }
